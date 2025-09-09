de61425bad2b525b0492ab54e5ba30d3dd25bf7a
P.Bandiera Paiva <paiva@unifesp.br>
2025-09-09 11:25:03 -0300

    inserido lacos.c

---
+++ LinguagemC/lacos.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int i;
+
+    printf("\nCom for:\n");
+    for(int j=0; j<11; j++) printf("%d\n", j); 
+
+    printf("\nCom while:\n");
+    i=0;
+    while(i<11){
+        printf("%d\n", i);
+        i++;
+    }
+
+    printf("\nCom do-while:\n");
+    i=0;
+    do 
+        printf("%d\n", i++); 
+    while(i<11);
+
+}
